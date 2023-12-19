14ba71cb880903ec1ad30eb1d579493449cea65f
Gilles TOURREAU <gilles.tourreau@pos-informatique.fr>
2023-12-19 22:54:56 +0100

    Add raw JS sample.

---
+++ communication-react-bundle.src.js
@@ -0,0 +1,6 @@
+import React from 'react'
+
+export let createElement = React.createElement;
+export let useState = React.useState;
+export { createRoot } from 'react-dom/client';
+export { MessageThread } from '@azure/communication-react';
